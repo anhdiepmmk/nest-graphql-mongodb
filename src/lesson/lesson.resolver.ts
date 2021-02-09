9561c261d61b228421ee1b18bdee5e0fca88b483
Pham ngoc diep <n08ni.dieppn@gmail.com>
2021-02-09 18:34:11 +0700

    feat: implement Resolve Field

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,5 +1,14 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { StudentService } from '../student/student.service';
 import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
@@ -7,7 +16,10 @@ import { LessonType } from './lesson.type';
 // eslint-disable-next-line prettier/prettier
 @Resolver(of => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService,
+  ) {}
 
   // eslint-disable-next-line prettier/prettier
   @Query(returns => LessonType)
@@ -36,5 +48,12 @@ export class LessonResolver {
   ) {
     const { lessonId, studentIds } = assignStudentsToLessonInput;
     return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    // console.log(lesson);
+    const students = lesson.students || [];
+    return this.studentService.getManyStudents(students);
   }
 }
